250270f3e96c74c20493b8de91a7a5a9aac9b9cf
huangdgithub <377053020@qq.com>
2018-02-28 18:14:42 +0800

    整理学习笔记

---
+++ spring-boot-web/src/main/java/com/neo/domain/UserRepository.java
@@ -0,0 +1,12 @@
+package com.neo.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUserName(String userName);
+
+    User findByUserNameOrEmail(String username, String email);
+
+    User findByNickName(String nickName);
+}
